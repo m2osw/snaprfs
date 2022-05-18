8e1fc626b1f23c9c971f41972c686bfd23dcc9c4
AlexisWilke <alexis@m2osw.com>
2022-05-17 18:14:08 -0700

    Cleaned up the cmake file.

--- cmake/SnapRFSConfig.cmake
+++ cmake/SnapRFSConfig.cmake
@@ -1,11 +1,20 @@
-# Copyright (c) 2019  Made to Order Software Corp.  All Rights Reserved
+# - Find SnapRFS (Remote File System)
+#
+# SNAPRFS_FOUND        - System has SnapRFS
+# SNAPRFS_INCLUDE_DIR  - The SnapRFS include directories
+# SNAPRFS_LIBRARY      - The libraries needed to use SnapRFS
+# SNAPRFS_DEFINITIONS  - Compiler switches required for using SnapRFS
+#
+# License:
+#
+# Copyright (c) 2011-2022  Made to Order Software Corp.  All Rights Reserved
 #
 # https://snapwebsites.org/project/snaprfs
 # contact@m2osw.com
-# 
-# This program is free software; you can redistribute it and/or modify
+#
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -14,25 +23,14 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# - Try to find SnapRFS
-#
-# Once done this will define
-#
-# SNAPRFS_FOUND        - System has SnapRFS
-# SNAPRFS_INCLUDE_DIR  - The SnapRFS include directories
-# SNAPRFS_LIBRARY      - The libraries needed to use SnapRFS (none)
-# SNAPRFS_DEFINITIONS  - Compiler switches required for using SnapRFS (none)
-#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 find_path(
     SNAPRFS_INCLUDE_DIR
         snaprfs/rfs.h
 
     PATHS
-        $ENV{SNAPRGS_INCLUDE_DIR}
+        ENV SNAPRFS_INCLUDE_DIR
 )
 
 find_library(
@@ -40,7 +38,8 @@ find_library(
         snaprfs
 
     PATHS
-        $ENV{SNAPRFS_LIBRARY}
+        ${SNAPRFS_LIBRARY_DIR}
+        ENV SNAPRFS_LIBRARY
 )
 
 mark_as_advanced(
@@ -49,17 +48,14 @@ mark_as_advanced(
 )
 
 set(SNAPRFS_INCLUDE_DIRS ${SNAPRFS_INCLUDE_DIR})
-set(SNAPRFS_LIBRARIES    ${SNAPRFS_LIBRARY}    )
+set(SNAPRFS_LIBRARIES    ${SNAPRFS_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-
-# handle the QUIETLY and REQUIRED arguments and set SNAPRFS_FOUND to TRUE
-# if all listed variables are TRUE
 find_package_handle_standard_args(
     SnapRFS
-    DEFAULT_MSG
-    SNAPRFS_INCLUDE_DIR
-    SNAPRFS_LIBRARY
+    REQUIRED_VARS
+        SNAPRFS_INCLUDE_DIR
+        SNAPRFS_LIBRARY
 )
 
 # vim: ts=4 sw=4 et
